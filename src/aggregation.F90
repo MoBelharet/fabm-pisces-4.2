3fd8137e8d77cc0a88b083cdb011a8697c88c6fe
BELHARET MOKRANE <belharetm@belenoslogin0.belenoshpc.meteo.fr>
2024-01-31 12:26:05 +0100

    updating the aggregation.F90 file

--- src/aggregation.F90
+++ src/aggregation.F90
@@ -61,14 +61,14 @@ contains
 
          zfact = xstep * xdiss
          !  Part I : Coagulation dependent on turbulence
-         zagg1 = 25.9  * zfact * poc * poc    ! Jorn Eq 39, POC^2 shear term, value of 25.9 matches a6 in paper
-         zagg2 = 4452. * zfact * poc * goc    ! Jorn Eq 39, POC*GOC shear term, value of 4452. matches a7 in paper
+         zagg1 = 12.5  * zfact * poc * poc    ! Jorn Eq 39, POC^2 shear term, value of 25.9 matches a6 in paper
+         zagg2 = 169.7 * zfact * poc * goc    ! Jorn Eq 39, POC*GOC shear term, value of 4452. matches a7 in paper
 
          ! Part II : Differential settling
 
          !  Aggregation of small into large particles
-         zagg3 =  47.1 * xstep * poc * goc    ! Jorn Eq 39, value of 47.1 matches a9 in paper, but a8 expected per Eq 39
-         zagg4 =  3.3  * xstep * poc * poc    ! Jorn Eq 39, value of 3.3 matches a8 in paper, but a9 expected per Eq 39
+         zagg3 =  8.63 * xstep * poc * goc    ! Jorn Eq 39, value of 47.1 matches a9 in paper, but a8 expected per Eq 39
+         zagg4 =  132.8  * xstep * poc * poc    ! Jorn Eq 39, value of 3.3 matches a8 in paper, but a9 expected per Eq 39
 
          zagg   = zagg1 + zagg2 + zagg3 + zagg4
          zaggfe = zagg * sfe / ( poc + rtrn )
@@ -77,14 +77,14 @@ contains
          ! 1st term is shear aggregation of DOC-DOC
          ! 2nd term is shear aggregation of DOC-POC
          ! 3rd term is differential settling of DOC-POC
-         zaggdoc  = ( ( 0.369 * 0.3 * doc + 102.4 * poc ) * zfact       &   ! Jorn: Eq 36a, values of 0.369 and 102.4 match a1=0.37 and a2=102 in paper, respectively
-         &            + 2.4 * xstep * poc ) * 0.3 * doc                     ! Jorn: presumably POC part of Eq 36c, but value of 2.4 does not match a4=5095 given in paper
+         zaggdoc  = ( ( 12.0 * 0.3 * doc + 9.05 * poc ) * zfact       &   ! Jorn: Eq 36a, values of 0.369 and 102.4 match a1=0.37 and a2=102 in paper, respectively
+         &            + 2.49 * xstep * poc ) * 0.3 * doc                     ! Jorn: presumably POC part of Eq 36c, but value of 2.4 does not match a4=5095 given in paper
          ! transfer of DOC to GOC : 
          ! 1st term is shear aggregation
          ! 2nd term is differential settling 
-         zaggdoc2 = ( 3.53E3 * zfact + 0.1 * xstep ) * goc * 0.3 * doc  ! Jorn: Eq 36b? value of 3.53E3 matches a3 in paper. GOC*DOC Brownian part with scale factor 0.1 not in paper
+         zaggdoc2 = ( 1.94 * zfact + 1.37 * xstep ) * goc * 0.3 * doc  ! Jorn: Eq 36b? value of 3.53E3 matches a3 in paper. GOC*DOC Brownian part with scale factor 0.1 not in paper
          ! tranfer of DOC to POC due to brownian motion
-         zaggdoc3 =  114. * 0.3 * doc *xstep * 0.3 * doc                ! Jorn: Eq 36c, DOC part only, value of 114. matches a5 in paper
+         zaggdoc3 =  (127.8 * 0.3 * doc + 725.7 * poc) *xstep * 0.3 * doc                ! Jorn: Eq 36c, DOC part only, value of 114. matches a5 in paper
 
          !  Update the trends
          _ADD_SOURCE_(self%id_poc, - zagg + zaggdoc + zaggdoc3)
